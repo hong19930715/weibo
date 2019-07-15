00d7c7ce159253a17dfb823d7cde5e1099f86ed2
hong <354381340@qq.com>
2019-07-15 16:37:07 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,8 +12,20 @@ class UsersController extends Controller
         return view('users.create');
     }
 
+    // User $user 接收model参数
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    // Request $request 接收表单提交的参数
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' =>'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
