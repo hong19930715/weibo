d100ad09d003c7efd187bbd0c4031ccbcbecb8aa
hong <354381340@qq.com>
2019-07-16 14:18:39 +0800

    登录成功后导航逻辑

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class SessionsController extends Controller
 {
@@ -21,11 +22,11 @@ class SessionsController extends Controller
         if (Auth::attempt($credentials)) {
             // 登录成功后的相关操作
             session()->flash('success', '欢迎回来！');
-            return redirect()->route('user.show', [Auth::user()]);
+            return redirect()->route('users.show', [Auth::user()]);
         } else {
             // 登录失败后的相关操作
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-            return redirect()->back()->withIput();
+            return redirect()->back()->withInput();
         }
 
         return;
