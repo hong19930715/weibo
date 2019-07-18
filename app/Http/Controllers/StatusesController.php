f6d6fb5c2aac2772b6d0a5ef16354a4139e136c0
hong <354381340@qq.com>
2019-07-18 19:21:18 +0800

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -29,4 +29,14 @@ class StatusesController extends Controller
         session()->flash('success', '发布成功！');
         return redirect()->back();
     }
+
+    public function destroy(Status $status)
+    {
+        // 授权检测
+        $this->authorize('destroy', $status);
+        // 删除数据
+        $status->delete();
+        session()->flash('success', '微博已被成功删除！');
+        return redirect()->back();
+    }
 }
